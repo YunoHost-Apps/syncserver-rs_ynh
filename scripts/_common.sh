06153320003bf2d7821d67cc45c7ceaaf0bd0d6d
orhtej2 <2871798+orhtej2@users.noreply.github.com>
2026-07-17 10:37:06 +0200

    Switch to postgres
    MariaDB->Psql continued
    
    zzz
    
    zzz
    
    updated artifacts
    
    Fixup

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -17,27 +17,26 @@ myynh_build() {
 		ynh_exec_as_app "$install_dir/venv/bin/pip" install -r requirements.txt
 		ynh_exec_as_app "$install_dir/venv/bin/pip" install -r requirements-tokenserver.txt
 		ynh_print_info "Seeding the databases..."
-		mysql --version
 		./diesel --version
 		# syncstorage db
-		./diesel --database-url "mysql://$db_user:${db_pwd}@localhost/$db_name" migration --migration-dir syncstorage-mysql/migrations list
-		./diesel --database-url "mysql://$db_user:${db_pwd}@localhost/$db_name" migration --migration-dir syncstorage-mysql/migrations run
-		./diesel --database-url "mysql://$db_user:${db_pwd}@localhost/$db_name" migration --migration-dir syncstorage-mysql/migrations list
+		./diesel --database-url "postgres://$db_user:${db_pwd}@localhost/$db_name" migration --migration-dir syncstorage-postgres/migrations list
+		./diesel --database-url "postgres://$db_user:${db_pwd}@localhost/$db_name" migration --migration-dir syncstorage-postgres/migrations run
+		./diesel --database-url "postgres://$db_user:${db_pwd}@localhost/$db_name" migration --migration-dir syncstorage-postgres/migrations list
 
 		# tokenserver db
-		./diesel --database-url "mysql://$db_user:${db_pwd}@localhost/$db_name_tokenserver" migration --migration-dir tokenserver-mysql/migrations list
-		./diesel --database-url "mysql://$db_user:${db_pwd}@localhost/$db_name_tokenserver" migration --migration-dir tokenserver-mysql/migrations run
-		./diesel --database-url "mysql://$db_user:${db_pwd}@localhost/$db_name_tokenserver" migration --migration-dir tokenserver-mysql/migrations list
+		./diesel --database-url "postgres://$db_user:${db_pwd}@localhost/$db_name_tokenserver" migration --migration-dir tokenserver-postgres/migrations list
+		./diesel --database-url "postgres://$db_user:${db_pwd}@localhost/$db_name_tokenserver" migration --migration-dir tokenserver-postgres/migrations run
+		./diesel --database-url "postgres://$db_user:${db_pwd}@localhost/$db_name_tokenserver" migration --migration-dir tokenserver-postgres/migrations list
 
-		if [[ -z ${YNH_APP_UPGRADE_TYPE:-} ]]
+		if [[ -z ${YNH_APP_UPGRADE_TYPE:-} ]] || ynh_app_upgrading_from_version_before 0.23.3~ynh9
 		then
-		# Add a node on install only
-		pushd "tools/tokenserver"
-			ynh_hide_warnings ynh_exec_as_app \
-				SYNC_TOKENSERVER__DATABASE_URL="mysql://$db_user:${db_pwd}@localhost/$db_name_tokenserver" \
-					$install_dir/venv/bin/python add_node.py "https://${domain%%+(/)}" 10
-		popd
-	fi
+			# Add a node on install only
+			pushd "tools/tokenserver"
+				ynh_hide_warnings ynh_exec_as_app \
+					SYNC_TOKENSERVER__DATABASE_URL="postgres://$db_user:${db_pwd}@localhost/$db_name_tokenserver" \
+						$install_dir/venv/bin/python add_node.py "https://${domain%%+(/)}" 10
+			popd
+		fi
 	popd
 
 	ynh_safe_rm "$install_dir/.cache"
